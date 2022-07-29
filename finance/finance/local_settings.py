d7b9a353c7bb70d47769d19c8551983664a2a95d
SergiiPachkovskyi <SergiiPachkovskyi@gmail.com>
2022-07-29 17:31:53 +0300

    fix req

--- finance/finance/local_settings.py
+++ finance/finance/local_settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
+    # 'debug_toolbar',
     'fin.apps.FinConfig',
 ]
 
@@ -44,5 +44,5 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
